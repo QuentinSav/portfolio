31df9453de1a9e26198dc5bd3a9fe97d2437dd7d
QuentinSav <quentin.savary@bluewin.ch>
2023-07-09 00:33:29 +0200

    Added carousel

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     $("#push-btn-menu").on("click", function(){
-        $("#lgMenu").css("box-shadow", "5px 5px 10px rgb(94, 158, 123)");
+        $("#lgMenu").css("box-shadow", "2px 2px 40px rgb(25, 25, 25)");
         $("#lgMenu").addClass("enter");
     });
         
     $("#exit").on("click", function(){
-        $("#lgMenu").css("box-shadow", "0px 0px 0px rgb(64, 64, 64)");
+        $("#lgMenu").css("box-shadow", "0px 0px 0px rgb(25, 25, 25)");
         $("#lgMenu").removeClass("enter");
     });
 });
